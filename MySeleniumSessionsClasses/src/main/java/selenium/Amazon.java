63d314345274a112f1cc9f78c3affbd87e65bee5
demotesting221198 <demotesting221198@gmail.com>
2020-12-12 14:37:51 +0530

    Adding source code file

--- MySeleniumSessionsClasses/src/main/java/selenium/Amazon.java
+++ MySeleniumSessionsClasses/src/main/java/selenium/Amazon.java
@@ -1,21 +1,143 @@
 package selenium;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.JavascriptExecutor;
+import java.net.URL;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.Platform;
 
 public class Amazon {
+	
+	public static final String AUTOMATE_USERNAME = "demotesting4";
+	
+	public static final String AUTOMATE_ACCESS_KEY = "j9hzk38LRuEQwB8coPqC";
+	
+	public static final String URL = "https://" + AUTOMATE_USERNAME + ":" + AUTOMATE_ACCESS_KEY + "@hub-cloud.browserstack.com/wd/hub";
 
-	public static void main(String[] args) {
+	public static void main(String[] args)throws Exception {
 		// TODO Auto-generated method stub
+		
+		
+		DesiredCapabilities caps = new DesiredCapabilities();
+		
+		caps.setCapability("os_version", "10");
+		
+	    caps.setCapability("resolution", "1920x1080");
+	    
+	    caps.setCapability("browser", "Chrome");
+	    
+	    caps.setCapability("browser_version", "latest");
+	    
+	    caps.setCapability("os", "Windows");
+	    
+	    caps.setCapability("name", "BStack-[Java] Sample Test"); // test name
+	    
+	    caps.setCapability("build", "BStack Build Number 1"); // CI/CD job or build name
+	    
+	    WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
 
-		System.setProperty("webdriver.chrome.driver", "/Users/bhagyesh/Downloads/chromedriver");
+		//System.setProperty("webdriver.chrome.driver", "/Users/bhagyesh/Downloads/chromedriver");
 		
-		WebDriver driver = new ChromeDriver();
+		//WebDriver driver = new ChromeDriver();
 		
-		driver.get("http://www.google.com");
+		driver.manage().window().maximize();
 		
-		System.out.println(driver.getTitle());
+		driver.get("https://www.amazon.com/");
 		
+		WebElement search = driver.findElement(By.id("twotabsearchtextbox"));
+		
+		search.sendKeys("iPhone X");
+		
+		search.sendKeys(Keys.ENTER);
+		
+		WebDriverWait wait1 = new WebDriverWait(driver,20);
+		
+		WebElement ios = wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"p_n_feature_twenty_browse-bin/17881878011\"]/span/a/span")));
+		
+		ios.click();
+		
+		/*
+		WebDriverWait wait2 = new WebDriverWait(driver,20);
+		
+		WebElement done = wait2.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[4]/div/div/div[2]/span/span/span/button")));
+		
+		done.click();
+		*/
+		
+		WebDriverWait wait3 = new WebDriverWait(driver,20);
+		
+		WebElement sortby = wait3.until(ExpectedConditions.visibilityOfElementLocated(By.className("a-dropdown-prompt")));
+		
+		sortby.click();
+		
+		WebDriverWait wait4 = new WebDriverWait(driver,20);
+		
+		WebElement HightoLow = wait4.until(ExpectedConditions.visibilityOfElementLocated(By.id("s-result-sort-select_2")));
+		
+		HightoLow.click();
+		
+		System.out.println("Name");
+		
+		
+		try {
+			
+			WebDriverWait wait5 = new WebDriverWait(driver, 10);
+			
+			List<WebElement> names = wait5.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.className("a-size-medium")));
+			
+			for(WebElement webElement: names) {
+				
+			String temp = webElement.getText();
+			
+			System.out.println(temp); 
+			
+			}
+			
+			}
+		
+			catch(Exception e) { 
+				
+				System.out.println(e.getMessage());
+			
+			}
+		
+		
+			System.out.println("Links");
+		
+			try {
+			
+			WebDriverWait wait5 = new WebDriverWait(driver, 10);
+			
+			List<WebElement> links = wait5.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("/html/body/div[1]/div[2]/div[1]/div[2]/div/span[3]/div[2]/div/div/span/div/div/div[2]/div[2]/div/div[1]/div/div/div[1]/h2/a")));
+			
+			for(WebElement webElement: links) {
+				
+			String temp = webElement.getAttribute("href");
+			
+			System.out.println(temp); 
+			
+			}
+			
+			}
+		
+			catch(Exception e) { 
+				
+				System.out.println(e.getMessage());
+			
+			}
+			
+		
+
+		driver.quit();
 		
 		
 	}
